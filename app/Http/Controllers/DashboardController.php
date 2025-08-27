be2dffbad0e159982f8384ec84cf627f8273f382
Júnior Soares <juniorsoaresti@gmail.com>
2025-08-27 18:15:47 -0400

    Order dashboard questions by likes and unlikes, update query logic, and add test for ordering.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,13 @@ class DashboardController extends Controller
     public function __invoke()
     {
         return view('dashboard', [
-            'questions' => Question::withSum('votes', 'like')->withSum('votes', 'unlike')->paginate(10),
+            'questions' => Question::query()
+                ->withSum('votes', 'like')->withSum('votes', 'unlike')
+                ->orderByRaw('
+                    case when votes_sum_like is null then 0 else votes_sum_like end desc,
+                    case when votes_sum_unlike is null then 0 else votes_sum_unlike end
+                ')
+                ->paginate(10),
         ]);
     }
 }
