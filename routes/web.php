a8c6c9036c99cd1bcd6fde87d5dd3816022b9e49
Júnior Soares <juniorsoaresti@gmail.com>
2025-09-01 09:57:49 -0400

    chore: upgrade v10 -> v11

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (app()->isLocal()) {
-        auth()->loginUsingId(1);
-
-        return to_route('dashboard');
+        //        auth()->loginUsingId(1);
+        //
+        //        return to_route('dashboard');
     }
 
     return view('welcome');
